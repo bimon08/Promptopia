8d5f06e587905255343d658f39807900634709a9
JyrwaJR <jyrwaboys@gmail.com>
2024-02-27 19:08:10 +0530

    fix font

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,14 @@
 // prettier.config.js
 module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+  importOrder: ["^@component/(.*)$", "^@src/(.*)$", "^@shared/(.*)$", "^[./]"],
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import Provider from "@src/components/Provider";
 import "./globals.css";
+import "./style.css";
+import { Toaster } from "sonner";
+import { Poppins } from "next/font/google";
 
-import { Toaster, toast } from "sonner";
-
+// If loading a variable font, you don't need to specify the font weight
+const inter = Poppins({ weight: "400", display: "swap", subsets: ["latin"] });
 export const metadata = {
   title: "Build and Grow Together",
   description:
@@ -16,15 +19,14 @@ interface RootLayoutPropstype {
 const RootLayout = ({ children }: RootLayoutPropstype) => {
   return (
     <html lang="en">
-      <body>
+      <body className={"font-satoshi"}>
         <Provider>
           <div className="main">
             <div className="gradient" />
           </div>
-
           <main className="app">{children}</main>
+          <Toaster />
         </Provider>
-        <Toaster />
       </body>
     </html>
   );
